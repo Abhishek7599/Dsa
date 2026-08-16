0c1a9a74ec4aa53ee78962211d349abe70fb192e
Abhishek Kumar <selfish.abhishek2602@gmail.com>
2026-08-17 00:13:23 +0530

    Time: 0 ms (100%), Space: 14.9 MB (24.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         unordered_map <int,int> map;
-        vector <int> ans;
+        vector<int> ans;
 
         for(int i=0;i<n;i++){
             int first = nums[i];
